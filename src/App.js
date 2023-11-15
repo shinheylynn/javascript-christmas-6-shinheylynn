6465b2bdaf424e30949cf76f7e2755f7231d53e4
shinheylynn <heylub@gmail.com>
2023-11-15 19:06:31 +0900

    feat: 날짜, 메뉴 input 받아오기
    - OutputView.printGetDate(): 날짜 타이틀 및 날짜 입력값 출력
    - OutputView.printGetMenu(): 메뉴 타잉틀 및 메뉴 입력값 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import TitleView from "./views/TitleView";
 import NoBenefitView from "./views/NoBenefitView";
+import OutputView from "./views/OutputView";
 import UserController from "./controllers/UserController";
 
 class App {
@@ -19,7 +20,16 @@ class App {
 		await UserController.inputMenu(); // 예외 테스트 - 주문 예외 테스트
 	}
 
-	async run() {}
+	async run() {
+		const date = await OutputView.printGetDate();
+		const inputMenu = await OutputView.printGetMenu();
+		const parsedInputMenu = await this.menuController.parseMenuAndQuantity(
+			inputMenu
+		);
+		await Console.print(
+			`12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`
+		);
+	}
 }
 
 export default App;
---
+++ src/views/OutputView.js
@@ -0,0 +1,22 @@
+import User from "../controllers/UserController.js";
+import MenuController from "../controllers/MenuController.js";
+import { Console } from "@woowacourse/mission-utils";
+import { OUTPUT_TITLE, OUTPUT_MESSAGE } from "../constants/Messages.js";
+
+const OutputView = {
+	async printGetDate() {
+		const date = await User.inputDate();
+		Console.print(`${OUTPUT_MESSAGE.GET_DATE}\n${date}`);
+
+		return date;
+	},
+
+	async printGetMenu() {
+		const inputMenu = await User.inputMenu();
+		Console.print(`${OUTPUT_MESSAGE.GET_MENU}\n${inputMenu}`);
+
+		return inputMenu;
+	},
+};
+
+export default OutputView;
